56b4c1940fe96099a8b6459805d8febe7654e40d
romanmusiyowskij248@gmail.com <Igor130498>
2020-03-19 00:41:37 +0100

    Created userApi.js (login,register)

---
+++ src/services/userApi.js
@@ -0,0 +1,61 @@
+import axios from 'axios'
+import URLS from "../shared/Urls.constants";
+import {
+    loginFailure,
+    loginRequest,
+    loginSuccess,
+    registerFailure,
+    registerRequest,
+    registerSuccess
+} from "../actions/auth/actions";
+import history from "../shared/history";
+
+const userApi ={
+    login,
+    register
+};
+
+function login(usernameOrEmail, password ) {
+    return dispatch => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+
+        const body = JSON.stringify({usernameOrEmail, password});
+        dispatch(loginRequest(body));
+
+        axios.post(URLS.apiAuth+'/signin', body, config)
+            .then(
+                res => {
+                    dispatch(loginSuccess(res));
+                })
+            .catch(
+                error => {dispatch(loginFailure(error))});
+    }
+}
+
+function register(user){
+    return dispatch => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+
+        const body = JSON.stringify(user);
+        dispatch(registerRequest(body));
+
+        axios.post(URLS.apiAuth+'/signup', body, config)
+            .then(
+                res => {
+                    dispatch(registerSuccess(res));
+                    history.push('/signIn');
+                })
+            .catch(
+                error => {dispatch(registerFailure(error))});
+    }
+}
+
+export default userApi
