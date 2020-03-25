b77c1cea41c25154b4972a29c2de9266ac76ea75
romanmusiyowskij248@gmail.com <Igor130498>
2020-03-25 17:03:05 +0100

    Code review

--- src/shared/setAuthToken.js
+++ src/shared/setAuthToken.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 export default function setAuthToken(token) {
-    console.log('MSG from interceptor');
     if (token){
-        console.log('MSG from INTERCETOR')
-        console.log(token);
         axios.defaults.headers.common['Authorization'] = token;
     }else {
         delete axios.defaults.headers.common.Authorization;
