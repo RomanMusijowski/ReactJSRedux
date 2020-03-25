b40a56d1a51604a3e8c85e17dd071efec8cddebc
romanmusiyowskij248@gmail.com <Igor130498>
2020-03-25 17:05:36 +0100

    Code review, added dynamic behavior

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from "react-router-dom";
 import SingInLinks from "./SignInLinks";
 import SingOutLinks from "./SignOutLinks";
 import {connect} from "react-redux";
 
 
-const Navbar = (props) => {
-
-    const {userInfo} = props;
-    // console.log(this.props);
-    console.log('User information is here')
-    console.log(userInfo);
-
-    return(
-        <nav className="nav-wrapper grey darken-3">
-            <div className="container">
-                <Link to="/" className="brand-logo">TheBest</Link>
+class Navbar extends Component{
+    navbarLinks(){
+        if(this.props.authenticated){
+            return [
                 <SingInLinks/>
+            ];
+        } else {
+            return [
                 <SingOutLinks/>
-            </div>
-        </nav>
-    )
+            ];
+        }
+    }
+
+    render() {
+         return (
+            <nav className="nav-wrapper grey darken-3">
+                <div className="container">
+                    <Link to="/" className="brand-logo">TheBest</Link>
+                    {this.navbarLinks()}
+                </div>
+            </nav>
+        )
+    }
 };
 
 const mapStateToProps = (state) => {
     return {
-        userInfo: state.auth.userInfo
+        authenticated: state.auth.authenticated
     }
 };
 
