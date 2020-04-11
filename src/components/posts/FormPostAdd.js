2bb0fcc993a8a4478817a1d1147b9f3b9ebae4e0
Kamil Machanski <kamilmachanski@gmail.com>
2020-04-11 15:57:21 +0200

    add Avatar and icons for FormPostAdd

--- src/components/posts/FormPostAdd.js
+++ src/components/posts/FormPostAdd.js
@@ -1,28 +1,41 @@
 import React ,{Component} from "react";
+import Container from "@material-ui/core/Container";
+import InsertPhotoRoundedIcon from '@material-ui/icons/InsertPhotoRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+
 
 class FormPostAdd extends Component {
 
     render() {
         return (
-            <div className="container">
-                <form className="white" onSubmit={this.handleSubmit}>
-                    <div className="container">
+            <Container maxWidth="sm">
+                <form onSubmit={this.handleSubmit}>
                         <div className="card grey lighten-3">
                             <div className="card-content black-text">
+                                <Button href={"/#"}><Avatar /></Button>
                                 <span className="card-title">
-                                    <h8>Stwórz post</h8>
+                                    <h8>Stwórz post + nazwa użytkownika</h8>
+
                                  </span>
-                                <textarea id="textarea1" className="materialize-textarea"></textarea>
+                                <div className="row">
+
+                                    <textarea className="materialize-textarea"></textarea>
+                                </div>
                             </div>
                             <div className="card-action">
                                 <div className="row">
-                                <input className="btn light-blue accent-1 right"  type="submit" value="Submit"/>
+                                    <Button href={"/#"}><Icon component={AccountCircleRoundedIcon} color="primary" fontSize={"large"} ></Icon></Button>
+                                    <Button href={"/#"}><Icon component={InsertPhotoRoundedIcon} color="primary" fontSize={"large"} ></Icon></Button>
+                                    <input className="btn light-red accent-1 right"  type="submit" value="Submit"/>
+
                                 </div>
                             </div>
                         </div>
-                    </div>
                 </form>
-            </div>
+            </Container>
 
         );
     }
