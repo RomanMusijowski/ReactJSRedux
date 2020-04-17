56b403da780db6ec6f83e8f125b1af1178d032bd
romanmusiyowskij248@gmail.com <Igor130498>
2020-04-17 19:39:22 +0200

    added eventReducer.js and fetchEventList

---
+++ src/store/reducers/eventReducer.js
@@ -0,0 +1,22 @@
+import {eventConstant} from "../../constans/eventConstant";
+import _ from 'lodash';
+
+const INITIAL_STATE = {
+
+};
+
+const eventReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case eventConstant.FETCH_LIST_OF_POSTS_SUCCESS:
+            const newEvents = _.mapKeys(action.payload.data.content, 'id');
+            return {...state, ...newEvents};
+
+        // case eventConstant.DELETE_POST:
+        //     //action.payload === id of a post to delete
+        //     return _.omit(state, action.payload);
+        default:
+            return state
+    }
+};
+
+export default eventReducer
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import authReducer from "./authReducer";
 import registerReducer from "./registerReducer"
+import eventReducer from "./eventReducer"
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    register: registerReducer
+    register: registerReducer,
+    event: eventReducer,
 });
 
 export default rootReducer
