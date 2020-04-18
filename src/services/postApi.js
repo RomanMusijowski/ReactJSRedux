1c927d84998df976dbb85a812c28f9b9295cf862
Kamil Machanski <kamilmachanski@gmail.com>
2020-04-19 00:07:07 +0200

    add get all comment axios metod

--- src/services/postApi.js
+++ src/services/postApi.js
@@ -3,7 +3,10 @@ import URLS from "../shared/Urls.constants";
 import {
     postLoadRequest,
     postLoadSuccess,
-    postLoadFailure
+    postLoadFailure,
+    commentsLoadRequest,
+    commentsLoadSuccess,
+    commentsLoadFailure
 } from "../actions/post/actions";
 import setAuthToken from "../shared/setAuthToken";
 import { withRouter } from "react-router";
@@ -21,3 +24,16 @@ export const getAllPostFriends = () => async (dispatch) =>{
             .catch((error) => {dispatch(postLoadFailure(error))})
 };
 
+/*****
+ * Return List Post comments
+ * id post
+ *
+ * @param id
+ * @returns {function(...[*]=)}
+ */
+export const getAllComments = (id/*, page = 1*/) => async (dispatch) =>{
+    dispatch(commentsLoadRequest('Comments loading have started.'));
+    axios.get(URLS.apiPost+'/'+id+'/comment')
+        .then((res) => {dispatch(commentsLoadSuccess(res ))})
+        .catch((error) => {dispatch(commentsLoadFailure(error))})
+};
