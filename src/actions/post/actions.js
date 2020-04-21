3e1c54e58c738923024da1ece8295d96d0ec7278
Kamil Machanski <kamilmachanski@gmail.com>
2020-04-21 18:51:07 +0200

    create postAdd actions

--- src/actions/post/actions.js
+++ src/actions/post/actions.js
@@ -5,6 +5,10 @@ export const postLoadRequest = (data) =>createAction(postConstants.POST_LOAD_REQ
 export const postLoadSuccess = (data) =>createAction(postConstants.POST_LOAD_SUCCESS, data);
 export const postLoadFailure = (error) =>createAction(postConstants.POST_LOAD_FAILURE, error);
 
+//export const postCreateRequest = (data) =>createAction(postConstants.POST_CREATE_REQUEST, data);
+export const postCreateSuccess = (data) =>createAction(postConstants.POST_CREATE_SUCCESS, data);
+export const postCreateFailure = (error) =>createAction(postConstants.POST_CREATE_FAILURE, error);
+
 export const commentsLoadRequest = (data) =>createAction(postConstants.COMMENT_LOAD_REQUEST, data);
 export const commentsLoadSuccess = (data) =>createAction(postConstants.COMMENT_LOAD_SUCCESS, data);
 export const commentsLoadFailure = (error) =>createAction(postConstants.COMMENT_LOAD_FAILURE, error);
