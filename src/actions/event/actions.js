856c41761024cbb659fd6a7420a0da900fea39d5
romanmusiyowskij248@gmail.com <Igor130498>
2020-04-30 18:31:58 +0200

    renaming

--- src/actions/event/actions.js
+++ src/actions/event/actions.js
@@ -5,5 +5,5 @@ import {eventConstant} from "../../constans/eventConstant";
 export const eventCreateSuccess = (data) => createAction(eventConstant.CREATE_SUCCESS, data);
 export const eventCreateFailure = (error) => createAction(eventConstant.CREATE_FAILURE, error);
 
-export const eventFetchListSuccess = (data) => createAction(eventConstant.FETCH_LIST_OF_POSTS_SUCCESS, data);
-export const eventFetchListFailure = (error) => createAction(eventConstant.FETCH_LIST_OF_POSTS_FAILURE, error);
+export const eventFetchListSuccess = (data) => createAction(eventConstant.FETCH_LIST_OF_EVENTS_SUCCESS, data);
+export const eventFetchListFailure = (error) => createAction(eventConstant.FETCH_LIST_OF_EVENTS_FAILURE, error);
