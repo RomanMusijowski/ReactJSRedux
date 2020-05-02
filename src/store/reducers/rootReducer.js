1e6bbff9f6d06feb47f642b2b00adf323d4bd517
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-02 19:02:54 +0200

    added userReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,14 +3,14 @@ import authReducer from "./authReducer";
 import postReducer from "./postReducer";
 import registerReducer from "./registerReducer"
 import eventReducer from "./eventReducer"
-
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     register: registerReducer,
     post: postReducer,
     event: eventReducer,
-
+    user: userReducer
 });
 
 export default rootReducer
