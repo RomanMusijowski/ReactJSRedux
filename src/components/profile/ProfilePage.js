d96fe6d2b3cf74a490b8cee8b899c551a6c0d4f6
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-02 19:04:50 +0200

    added fetchUserFriends, small changes

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -5,57 +5,66 @@ import ProfileWall from "./ProfileWall";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import FriendsCount from "./FriendsCount";
-import EventCount from "../events/EventCount"
 import InviteCount from "../events/InviteCount";
-import userApi from "../../services/userApi";
+import {fetchUserFriends} from "../../services/userApi";
 
 class ProfilePage extends Component {
 
     componentDidMount() {
-        this.props.fetchUserFriendsId(this.props.userInfo.id);
+        this.props.fetchUserFriends(this.props.userInfo.id)
     }
 
     render() {
+        const {userIsLoaded, userInfo, userList} = this.props;
         return (
-            <Container maxWidth="lg">
-                <ProfileArea username={this.props.userInfo.username}
-                             email={this.props.userInfo.email}
-                             firstName={this.props.userInfo.firstName}
-                             lastName={this.props.userInfo.lastName}
-                             phoneNumber={this.props.userInfo.phoneNumber}
-                             gender={this.props.userInfo.gender}/>
+            <div>
+                { userIsLoaded ? (
+                    <Container maxWidth="lg">
+                        <ProfileArea username={userInfo.username}
+                                     email={userInfo.email}
+                                     firstName={userInfo.firstName}
+                                     lastName={userInfo.lastName}
+                                     phoneNumber={userInfo.phoneNumber}
+                                     gender={userInfo.gender}/>
 
-                <Grid container spacing={2}>
-                    <Grid item
-                          xs={4}
-                          direction="column">
-                        <FriendsCount friends={this.props.userInfo.friends.length}/>
+                        <Grid container spacing={2}>
+                            <Grid item
+                                  xs={4}
+                                  direction="column">
+                                <FriendsCount friends={userList[userInfo.id].friends.length}/>
 
-                        <EventCount events={this.props.userInfo.invitedEvents !== null ?
-                            this.props.userInfo.invitedEvents :
-                            '0'}/>
+                                {/*<EventCount events={userInfo.invitedEvents !== null ?*/}
+                                {/*    userInfo.invitedEvents :*/}
+                                {/*    '0'}/>*/}
 
-                            <InviteCount invites={this.props.userInfo.invitedEvents !== null ?
-                            this.props.userInfo.invitedEvents :
-                            '0'}/>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <ProfileWall/>
-                    </Grid>
-                </Grid>
-            </Container>
+                                <InviteCount invites={userInfo.invitedEvents !== null ?
+                                    userInfo.invitedEvents :
+                                    '0'}/>
+                            </Grid>
+                            <Grid item xs={8}>
+                                <ProfileWall/>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                    ) : (
+                        <p>Page is loading</p>
+                    )
+                }
+            </div>
         );
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        userInfo: state.auth.userInfo
+        userInfo: state.auth.userInfo,
+        userIsLoaded: state.auth.userIsLoaded,
+        userList: state.user
     }
 };
 
 const mapDispatchToProps = {
-    fetchUserFriendsId: userApi.fetchUserFriendsId
+    fetchUserFriends: fetchUserFriends
 }
 
 export default connect(
