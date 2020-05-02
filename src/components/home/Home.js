24609a2ed43f2ffd09762fdb5d59b62044bb5a01
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-02 19:03:29 +0200

    adde fetchUserProfile()

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import userApi from "../../services/userApi";
 import FormPostAdd from "../../components/posts/FormPostAdd";
 import PostList from "../../components/posts/PostList";
+import {fetchUserProfile} from "../../services/userApi";
 
 class Home extends Component {
 
+    fetchUserProfile() {
+        this.props.fetchUserProfile(this.props.userInfo.id);
+    }
+
     render() {
         const {userIsLoaded} = this.props;
-
         return (
             <div>
                 {userIsLoaded ? (
                         <div>
+                            {this.fetchUserProfile()}
                             <FormPostAdd/>
                             <PostList/>
                         </div>
                     ) : (
-                    <p>Please a little bit more</p>
+                    <p>Please wait a little bit more.</p>
                     )
                 }
             </div>
@@ -27,12 +31,13 @@ class Home extends Component {
 
 
 const mapDispatchToProps = {
-    fetchLoadUser: userApi.fetchLoadUser,
+    fetchUserProfile: fetchUserProfile,
 };
 
 const mapStateToProps = (state) => {
     return {
         userIsLoaded: state.auth.userIsLoaded,
+        userInfo: state.auth.userInfo
     }
 }
 
