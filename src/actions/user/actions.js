33614240a7dc6b8b70bd36dd9507cd9950e349c8
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-02 18:57:05 +0200

    small changes

--- src/actions/user/actions.js
+++ src/actions/user/actions.js
@@ -6,6 +6,3 @@ export const userProfileLoadFailure = (error) => createAction(userConstants.USER
 
 export const userProfileUpdateSuccess = (data) => createAction(userConstants.USER_PROFILE_UPDATE_SECCESS, data)
 export const userProfileUpdateFailure = (error) => createAction(userConstants.USER_PROFILE_UPDATE_FAILURE, error)
-
-export const userFriendsIdSuccess = (data) => createAction(userConstants.USER_FRIENDS_ID_SUCCESS, data);
-export const userFriendsIdFailure = (error) => createAction(userConstants.USER_FRIENDS_ID_FAILURE, error);
