9f277f51615409845aa1454cf7f1713714c9fd55
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-03 17:23:18 +0200

    added new one

--- src/actions/user/actions.js
+++ src/actions/user/actions.js
@@ -6,3 +6,6 @@ export const userProfileLoadFailure = (error) => createAction(userConstants.USER
 
 export const userProfileUpdateSuccess = (data) => createAction(userConstants.USER_PROFILE_UPDATE_SECCESS, data)
 export const userProfileUpdateFailure = (error) => createAction(userConstants.USER_PROFILE_UPDATE_FAILURE, error)
+
+export const fetchUserEventsSuccess =(data) => createAction(userConstants.FETCH_USER_EVENTS_SUCCESS, data)
+export const fetchUserEventsFailure = (error) => createAction(userConstants.FETCH_USER_EVENTS_FAILURE, error)
