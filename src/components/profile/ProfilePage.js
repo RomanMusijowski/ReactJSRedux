ecf565f0cf9ab24c1a4de2344cc052bff1345d6e
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-07 13:13:21 +0200

    profilePage works for all users by userId as url param

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -1,67 +1,70 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import ProfileArea from "./ProfileArea";
 import ProfileWall from "./ProfileWall";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import FriendsCount from "./FriendsCount";
 import InviteCount from "./InviteCount";
-import {fetchUserEvents} from "../../services/userApi";
+import {fetchUserProfile} from "../../services/userApi";
 import EventCount from "./EventCount";
 
-class ProfilePage extends Component {
+const ProfilePage = (props) => {
 
-    componentDidMount() {
-        this.props.fetchUserEvents(this.props.userInfoId)
+    const dispatch = useDispatch();
+
+    const userId = props.match.params.id
+    const userList = useSelector((state)=> state.user);
+    const userInfoId = useSelector((state) => state.auth.userInfo.id)
+    const loggedInUser = new Boolean(Number(userId) === Number(userInfoId))
+
+    useEffect(() => {
+        if (!!loggedInUser){
+            dispatch(fetchUserProfile(userId))
+        }
+    }, [dispatch]);
+
+    const showProfilePage = function() {
+        const user = userList[userId]
+        if (user !== undefined ){
+            if (user.profileLoad && user.friendsLoad && user.eventsLoad){
+                return true
+            }
+        }else {
+            return false
+        }
     }
 
-    render() {
-        const {userIsLoaded, userInfoId, userList} = this.props;
-        return (
-            <div>
-                { userIsLoaded ? (
-                    <Container maxWidth="lg">
-                        <ProfileArea username={userList[userInfoId].username}
-                                     email={userList[userInfoId].email}
-                                     firstName={userList[userInfoId].firstName}
-                                     lastName={userList[userInfoId].lastName}
-                                     phoneNumber={userList[userInfoId].phoneNumber}
-                                     gender={userList[userInfoId].gender}/>
+    return (
+        <div>
+            { (showProfilePage()) ? (
+                <Container maxWidth="lg">
+                    <ProfileArea username={userList[userId].username}
+                                 email={userList[userId].email}
+                                 firstName={userList[userId].firstName}
+                                 lastName={userList[userId].lastName}
+                                 phoneNumber={userList[userId].phoneNumber}
+                                 gender={userList[userId].gender}/>
 
-                        <Grid container spacing={2}>
-                            <Grid item
-                                  xs={4}
-                                  direction="column">
-                                <FriendsCount friends={userList[userInfoId].friends}/>
-                                <EventCount events={userList[userInfoId].events}/>
-                                <InviteCount invites={userList[userInfoId].invitedEvents}/>
-                            </Grid>
-                            <Grid item xs={8}>
-                                <ProfileWall/>
-                            </Grid>
+                    <Grid container spacing={2}>
+                        <Grid item
+                              xs={4}
+                              direction="column">
+                            <FriendsCount friends={userList[userId].friends}/>
+                            <EventCount events={userList[userId].events}/>
+                            <InviteCount invites={userList[userId].invitedEvents}/>
                         </Grid>
-                    </Container>
-                    ) : (
-                        <p>Page is loading</p>
-                    )
-                }
-            </div>
-        );
-    }
+                        <Grid item xs={8}>
+                            <ProfileWall/>
+                        </Grid>
+                    </Grid>
+                </Container>
+                ) : (
+                    <p>The page is loading</p>
+                )
+            }
+        </div>
+    );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        userInfoId: state.auth.userInfo.id,
-        userIsLoaded: state.auth.userIsLoaded,
-        userList: state.user
-    }
-};
-
-const mapDispatchToProps = {
-    fetchUserEvents: fetchUserEvents
-}
-
-export default connect(
-    mapStateToProps, mapDispatchToProps
-)(ProfilePage);
+export default ProfilePage
