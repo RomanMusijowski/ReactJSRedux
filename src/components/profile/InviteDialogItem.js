c21140e373d053734b77fbc95adca8a3dd6be459
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-12 16:11:17 +0200

    added user & event details

--- src/components/profile/InviteDialogItem.js
+++ src/components/profile/InviteDialogItem.js
@@ -1,18 +1,27 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import ListItem from "@material-ui/core/ListItem";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import FolderIcon from "@material-ui/icons/Folder";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import React from "react";
+import React, {useEffect} from "react";
 import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import EventBusyIcon from '@material-ui/icons/EventBusy';
-import {acceptInvite, deleteInvite} from "../../services/userApi";
+import {acceptInvite, deleteInvite, fetchUserProfile} from "../../services/userApi";
+import {fetchEventList} from "../../services/eventApi";
 
 
 const InviteDialogItem = ({byUser, eventId, loggedInUserId}) => {
+
     const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch(fetchEventList())
+        dispatch(fetchUserProfile(byUser))
+    }, [dispatch]);
+    const listOfEvents = useSelector((state)=> state.event)
+    const listOfUsers = useSelector((state)=> state.user)
 
     const handleDeleteInvite = (eventId, userId) => {
         dispatch(deleteInvite(eventId, userId));
@@ -22,8 +31,19 @@ const InviteDialogItem = ({byUser, eventId, loggedInUserId}) => {
         dispatch(acceptInvite(eventId, userId, ));
     };
 
+    const showInvites = function () {
+        const user = listOfUsers[byUser]
+        const event = listOfEvents[eventId]
+        if (user === undefined || event === undefined){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     return(
         <ListItem alignItems="flex-start">
+            {showInvites() ? (
             <Grid container spacing={2}>
                 <Grid item>
                     <Avatar>
@@ -33,11 +53,17 @@ const InviteDialogItem = ({byUser, eventId, loggedInUserId}) => {
                 <Grid item xs={12} sm container>
                     <Grid item xs container direction="column" spacing={2}>
                         <Grid item md>
+                            <Typography variant="button" display="block" gutterBottom>
+                                {listOfUsers[byUser].firstName} {listOfUsers[byUser].lastName}
+                            </Typography>
                             <Typography gutterBottom variant="subtitle1">
-                                {byUser}
+                                {listOfEvents[eventId].name}
                             </Typography>
                             <Typography variant="body2" gutterBottom>
-                                {eventId}
+                                {listOfEvents[eventId].description}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                {listOfEvents[eventId].dateTime}
                             </Typography>
                         </Grid>
                     </Grid>
@@ -46,15 +72,18 @@ const InviteDialogItem = ({byUser, eventId, loggedInUserId}) => {
                     <IconButton edge="end"
                                 aria-label="join"
                                 onClick={()=>handleJoinInvite(eventId, loggedInUserId)}>
-                        <EventBusyIcon />
+                        <EventAvailableIcon />
                     </IconButton>
                     <IconButton edge="end"
                                 aria-label="delete"
                                 onClick={()=>handleDeleteInvite(eventId, loggedInUserId)}>
-                        <EventAvailableIcon />
+                        <EventBusyIcon />
                     </IconButton>
                 </Grid>
             </Grid>
+            ) : (
+                <p>Page is loading</p>
+            )}
         </ListItem>
     )
 }
