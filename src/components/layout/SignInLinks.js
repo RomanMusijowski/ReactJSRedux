4e00c2b80c854616113d27a784213ff5df14fb8c
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-16 14:05:07 +0200

    changed links places

--- src/components/layout/SignInLinks.js
+++ src/components/layout/SignInLinks.js
@@ -16,10 +16,10 @@ const SingInLinks = ({username, userId}) => {
     return(
         <div>
             <ul className="right">
-                <li><a onClick={handleLogout}>Log out</a></li>
-                <li><NavLink to={'/profile/'+ userId} className="btn btn-floating pink lighten-1">{username}</NavLink></li>
                 <li><NavLink to='/event' className="btn btn-floating pink lighten-1">EV</NavLink></li>
                 <li><NavLink to='/community' className="btn btn-floating pink lighten-1">COM</NavLink></li>
+                <li><NavLink to={'/profile/'+ userId} className="btn btn-floating pink lighten-1">{username}</NavLink></li>
+                <li><a onClick={handleLogout}>Log out</a></li>
             </ul>
         </div>
     )
