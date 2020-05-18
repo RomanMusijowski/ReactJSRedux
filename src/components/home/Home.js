2086054954356294f71b8ad1cf9ac0464bb8aa7d
Kamil Machanski <kamilmachanski@gmail.com>
2020-05-18 20:48:56 +0200

    change Home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,34 +1,65 @@
-import React, {Component} from 'react';
-import {connect} from "react-redux";
+import React, {Component, useEffect} from 'react';
+import {connect, useDispatch, useSelector} from "react-redux";
 
 import authApi from "../../services/authApi";
-import {getAllPostFriends, postsFriendsUnload} from "../../services/postApi";
+import {commentsUnload, getAllComments, getAllPostFriends, postsFriendsUnload} from "../../services/postApi";
 import FormPostAdd from "../../components/posts/FormPostAdd";
 import PostList from "../../components/posts/PostList";
+import Pagination from "@material-ui/lab/Pagination";
+import Container from "@material-ui/core/Container";
+import {fetchLastCreatedUsers} from "../../services/userApi";
 
 
-class Home extends Component {
+const Home = () => {
 
-
+/*
     componentDidMount() {
-        this.props.fetchLoadUser();
+        //this.props.fetchLoadUser();
         this.props.getAllPostFriends();
     }
 
     componentWillUnmount() {
         this.props.postsFriendsUnload();
     }
+*/
 
-    render() {
-        const {posts} = this.props;
-        const {userIsLoaded} = this.props;
+
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const [postsPerPage, setPostsPerPage] = React.useState(10);
+    //const [postss, setPosts] =  React.useState([]);
+    const userInfo = useSelector((state) => state.auth.userInfo);
+
+    //console.log(users)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        dispatch(getAllPostFriends());
+        if (userInfo !== undefined){dispatch(fetchLastCreatedUsers(userInfo.id))}
+
+    }, []);
+
+
+
+    const userIsLoaded = useSelector((state) => state.auth.userIsLoaded);
+    const posts = useSelector((state) => state.post);
+    const numberPages = useSelector((state) => state.post.numberPage);
+    //console.log(posts);
+    //console.log(numberPages);
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    //const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
 
         return (
             <div>
                 {userIsLoaded ? (
                         <div>
+
                           <FormPostAdd />
                           <PostList posts={posts} />
+                            <Container maxWidth="sm">
+                                <Pagination  count={3} page={1}   />
+                            </Container>
                         </div>
                     ) : (
                     <p>Please wait a little bit more.</p>
@@ -38,8 +69,8 @@ class Home extends Component {
             </div>
         );
     }
-}
 
+/*
 const mapStateToProps = (state) => {
     return {
         userIsLoaded: state.auth.userIsLoaded,
@@ -56,7 +87,7 @@ const mapDispatchToProps = {
     postsFriendsUnload
 
 };
+*/
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home
 
