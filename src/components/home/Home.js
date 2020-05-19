5dd96da0802590719319014909d1ffa5fa6630df
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-19 12:23:35 +0200

    deleted comments & fixed error during logout from profilePage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,6 @@
-import React, {Component, useEffect} from 'react';
-import {connect, useDispatch, useSelector} from "react-redux";
-
-import authApi from "../../services/authApi";
-import {commentsUnload, getAllComments, getAllPostFriends, postsFriendsUnload} from "../../services/postApi";
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {getAllPostFriends} from "../../services/postApi";
 import FormPostAdd from "../../components/posts/FormPostAdd";
 import PostList from "../../components/posts/PostList";
 import Pagination from "@material-ui/lab/Pagination";
@@ -12,30 +10,18 @@ import {fetchLastCreatedUsers} from "../../services/userApi";
 
 const Home = () => {
 
-/*
-    componentDidMount() {
-        //this.props.fetchLoadUser();
-        this.props.getAllPostFriends();
-    }
-
-    componentWillUnmount() {
-        this.props.postsFriendsUnload();
-    }
-*/
-
-
     const [currentPage, setCurrentPage] = React.useState(1);
     const [postsPerPage, setPostsPerPage] = React.useState(10);
-    //const [postss, setPosts] =  React.useState([]);
     const userInfo = useSelector((state) => state.auth.userInfo);
 
-    //console.log(users)
     const dispatch = useDispatch();
 
     useEffect(() => {
 
         dispatch(getAllPostFriends());
-        if (userInfo !== undefined){dispatch(fetchLastCreatedUsers(userInfo.id))}
+        if (userInfo !== undefined && userInfo !== null){
+            dispatch(fetchLastCreatedUsers(userInfo.id))
+        }
 
     }, []);
 
@@ -44,11 +30,8 @@ const Home = () => {
     const userIsLoaded = useSelector((state) => state.auth.userIsLoaded);
     const posts = useSelector((state) => state.post);
     const numberPages = useSelector((state) => state.post.numberPage);
-    //console.log(posts);
-    //console.log(numberPages);
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    //const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
 
         return (
             <div>
@@ -69,25 +52,6 @@ const Home = () => {
             </div>
         );
     }
-
-/*
-const mapStateToProps = (state) => {
-    return {
-        userIsLoaded: state.auth.userIsLoaded,
-        username: state.auth.username,
-        posts: state.post
-    }
-}
-
-
-
-const mapDispatchToProps = {
-    fetchLoadUser: authApi.fetchLoadUser,
-    getAllPostFriends,
-    postsFriendsUnload
-
-};
-*/
 
 export default Home
 
