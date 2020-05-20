91d4abdb5f549b2f256ac852d6fb9fac592a12eb
romanmusiyowskij248@gmail.com <Igor130498>
2020-05-20 22:55:17 +0200

    added state update after a post is added

--- src/components/posts/FormPostAdd.js
+++ src/components/posts/FormPostAdd.js
@@ -1,58 +1,117 @@
-import React ,{Component} from "react";
+import React from "react";
 import Container from "@material-ui/core/Container";
-import InsertPhotoRoundedIcon from '@material-ui/icons/InsertPhotoRounded';
-import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
-import Icon from "@material-ui/core/Icon";
 import Button from "@material-ui/core/Button";
 import Avatar from "@material-ui/core/Avatar";
+import {useDispatch, useSelector} from "react-redux";
+import {addPost} from "../../services/postApi";
+import {Link} from "react-router-dom";
+import CardHeader from '@material-ui/core/CardHeader';
+import Thumb from "../events/Thumb";
+import {makeStyles} from "@material-ui/core/styles";
 
-import { withStyles} from "@material-ui/core/styles";
-import {connect} from "react-redux";
-import {compose} from "redux";
+const useStyles = makeStyles((theme) => ({
+    input: {
+        display: 'none',
+    },
+}));
+
+const FormPostAdd = (props) =>  {
+    const [content, setContent] = React.useState("");
+    const [file, setFile] = React.useState(null);
+
+    const dispatch = useDispatch();
+    const classes = useStyles();
+
+    const userList = useSelector((state) => state.user);
+    const userInfoId = useSelector((state) => state.auth.userInfo.id);
+    const username = useSelector((state) => state.auth.userInfo.username);
+    const photo = useSelector((state) => state.auth.userInfo);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(addPost(userInfoId, content, file));
+
+        setContent("");
+        if(content.length > 0) {
+            window.alert("Success create post");
+            document.getElementById("create-course-form").reset();
+        }
+        else window.alert("create post is empty");
+    };
+
+    const showAvatar = function () {
+        if (userList[userInfoId] !== undefined ){
+            return [
+            <Avatar alt="Carlos"
+                    src={userList[userInfoId].photos[0].url}
+            />
+           ];
+        }else {
+            return [
+                <Avatar alt="Carlos"
+                />
+            ];
+        }
+
+    }
+
+    return (
+        <Container maxWidth="sm">
+            <form onSubmit={handleSubmit} id="create-course-form">
+                <div className="card light-green lighten-3">
+                    <div className="card-content black-text">
+                        <CardHeader
+                            avatar={
+                                showAvatar()
 
 
-class FormPostAdd extends Component {
 
-    render() {
-        const { classes } = this.props;
-        return (
-            <Container maxWidth="sm">
-                <form onSubmit={this.handleSubmit}>
-                        <div className="card light-green lighten-3">
-                            <div className="card-content black-text">
-                                <Button href={"/#"}><Avatar /><label className={classes.styleLabel}>{this.props.username}</label> </Button>
-                                <span className="card-title">
-                                    <h8>Create post</h8>
-                                 </span>
-                                <div className="row">
-                                    <textarea className="materialize-textarea"></textarea>
-                                </div>
-                            </div>
-                            <div className="card-action">
-                                <div className="row">
-                                    <Button href={"/#"}><Icon component={AccountCircleRoundedIcon} color="primary" fontSize={"large"} ></Icon></Button>
-                                    <Button href={"/#"}><Icon component={InsertPhotoRoundedIcon} color="primary" fontSize={"large"} ></Icon></Button>
-                                    <input className="btn light-red accent-1 right"  type="submit" value="Submit"/>
 
-                                </div>
-                            </div>
+                            }
+
+                            subheader={
+                                <Link to={'/profile/'+userInfoId}>{username}</Link>
+                            }
+                            />
+
+
+
+                        <div className="row">
+                                            <textarea className="materialize-textarea"
+                                                      name="content"
+                                                      label="Content"
+                                                      onChange={event => setContent(event.target.value)}
+                                            ></textarea>
                         </div>
-                </form>
-            </Container>
+                    </div>
+                    <div className="card-action">
+                        <div className="row">
+                            <input
+                                accept="image/*"
+                                className={classes.input}
+                                id="contained-button-file"
+                                multiple
+                                type="file"
+                                onChange={(event) => {
+                                    setFile( event.currentTarget.files[0]);
+                                }}/>
+                            <label htmlFor="contained-button-file">
+                                <Button variant="contained" color="primary" component="span">
+                                    Upload photo
+                                </Button>
+                            </label>
+                            <Thumb file={file} />
 
-        );
-    }
 
+                            <input className="btn light-red accent-1 right"   type="submit"
+                                   value="Create Post" />
+
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </Container>
+    );
 }
-
-const mapStateToProps = (state) => {
-    return {
-        username: state.auth.username
-    }
-};
-
-export default compose (withStyles({
-    styleLabel: {
-        marginLeft: "20px"
-    }
-}), connect(mapStateToProps))(FormPostAdd);
+export default FormPostAdd
--- src/services/postApi.js
+++ src/services/postApi.js
@@ -1,12 +1,31 @@
 import axios from 'axios';
 import URLS from "../shared/Urls.constants";
 import {
+    deletePostFailure,
+    deletePostSuccess,
+    postCreateFailure,
+    postCreateSuccess,
+    postLoadFailure,
     postLoadRequest,
     postLoadSuccess,
-    postLoadFailure
+    postUnload,
+    putPostFailure,
+    putPostSuccess,
+    userPostLikedListLoadFailure,
+    userPostLikedListLoadSuccess
 } from "../actions/post/actions";
-import setAuthToken from "../shared/setAuthToken";
-import { withRouter } from "react-router";
+import {
+    commentsCreateFailure,
+    commentsCreateSuccess,
+    commentsLoadFailure,
+    commentsLoadSuccess,
+    commentsLoadUnload,
+    deleteCommentFailure,
+    deleteCommentSuccess,
+    putCommentFailure,
+    putCommentSuccess
+} from "../actions/comment/actions";
+import {getAllPostUser} from "./userPostApi";
 
 
 /*****
@@ -21,3 +40,155 @@ export const getAllPostFriends = () => async (dispatch) =>{
             .catch((error) => {dispatch(postLoadFailure(error))})
 };
 
+/*****
+ * Create Post
+ *
+ * @param content
+ * @returns {function(...[*]=)}
+ */
+export const addPost = (userId, content, file) => async (dispatch) => {
+
+      //const body = JSON.stringify(content);
+
+      const formData = new FormData();
+      formData.append('content',content);
+      formData.append('files',file);
+
+      axios.post(URLS.apiPost, formData)
+          .then(res => {
+              dispatch(postCreateSuccess(res))
+              dispatch(getAllPostUser(userId))
+          })
+          .catch( error => {dispatch(postCreateFailure(error.response.data.message))})
+};
+
+export const getAllUserPostLiked = (postId) => async (dispatch) => {
+    axios.get(URLS.apiPost + '/' + postId + '/liked')
+        .then(res => {
+
+            dispatch(userPostLikedListLoadSuccess(res));
+        })
+        .catch(error => {
+            dispatch(userPostLikedListLoadFailure(error));
+        })
+}
+
+/*****
+ * Return List Post comments
+ * id post
+ *
+ * @param id
+ * @returns {function(...[*]=)}
+ */
+export const getAllComments = (id/*, page = 1*/) => async (dispatch) =>{
+    axios.get(URLS.apiPost+'/'+id+'/comment')
+        .then((res) => {dispatch(commentsLoadSuccess(res))}) // return data
+        .catch((error) => {dispatch(commentsLoadFailure(error))})
+};
+
+export const addComment = (postId ,content) => async (dispatch) => {
+
+    const body = JSON.stringify({content});
+/*
+    const formData = new FormData();
+    formData.append('content',content);
+*/
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    axios.post(URLS.apiPost+'/'+postId+'/comment', body, config)
+        .then(res => {
+            dispatch(commentsCreateSuccess(res))
+        })
+        .catch( error => {dispatch(commentsCreateFailure(error))})
+};
+
+export const commentsUnload = () => async (dispatch) =>{
+    dispatch(commentsLoadUnload())
+};
+
+export const postsFriendsUnload = () => async (dispatch) =>{
+    dispatch(postUnload())
+};
+
+export const postLike = (id) => async (dispatch) =>{
+    axios.get(URLS.apiPost+'/'+id+'/like')
+        .then(res => {console.log(res)})
+        .catch(error => {console.log(error)})
+};
+
+export const commentLike = (postId, commentId) => async (dispatch) =>{
+    axios.get(URLS.apiPost+'/'+postId+'/comment/'+commentId+'/like')
+        .then(res => {console.log(res)})
+        .catch(error => {console.log(error)})
+};
+
+export const deletePost = (postId, userId ) => async  (dispatch) =>{
+    axios.delete(URLS.apiPost + '/' + postId)
+        .then(res => {
+            dispatch(deletePostSuccess(res))
+            dispatch(getAllPostUser(userId))
+        })
+        .catch(error => {
+            dispatch(deletePostFailure(error))
+        })
+};
+
+export const deleteComment = (postId, commentId) => async  (dispatch) =>{
+    axios.delete(URLS.apiPost + '/' + postId + '/comment/' +commentId)
+        .then(res => {
+            dispatch(deleteCommentSuccess(res))
+        })
+        .catch(error => {
+            dispatch(deleteCommentFailure(error))
+        })
+};
+
+export const putPost = (id,userId, content) => async  (dispatch) =>{
+
+    const formData = new FormData();
+    formData.append('content',content);
+
+    axios.put(URLS.apiPost + '/' + id, formData)
+        .then(res => {
+            dispatch(putPostSuccess(res))
+            dispatch(getAllPostUser(userId))
+        })
+        .catch(error => {
+            dispatch(putPostFailure(error))
+        })
+};
+
+export const putComment = (postId, commentId, content) => async  (dispatch) =>{
+
+    const body = JSON.stringify({content});
+    /*
+        const formData = new FormData();
+        formData.append('content',content);
+    */
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    axios.put(URLS.apiPost + '/' + postId + '/comment/' +commentId, body, config)
+        .then(res => {
+            dispatch(putCommentSuccess(res))
+        })
+        .catch(error => {
+            dispatch(putCommentFailure(error))
+        })
+};
+
+
+/*
+export const getUserInfo = (userId) => async (dispatch) =>{
+    axios.get(URLS.apiUser+'/'+userId)
+        .then((res) => {dispatch(userInfoLoadSuccess(res ))})
+        .catch((error) => {dispatch(userInfoLoadFailure(error))})
+};
+*/
